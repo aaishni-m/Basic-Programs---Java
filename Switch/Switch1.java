48b29cd749d0c730a10c9f5d5cfdfad3203ad1d5
Aaishni Mulik <aaishni@hotmail.com>
2025-02-06 18:59:26 +0530

    Add files via upload

---
+++ Switch/Switch1.java
@@ -0,0 +1,11 @@
+class Switch1 
+{
+	public static void main(String[] args) 
+	{
+		int start=1;
+		while(start<=100){
+			System.out.print(start+" ");
+			start++;
+		}
+	}
+}
---
+++ Switch/Switch2.java
@@ -0,0 +1,11 @@
+class Switch2 
+{
+	public static void main(String[] args) 
+	{
+		char start = 'a';
+		while(start<='z'){
+			System.out.print(start+" ");
+			start++;
+		}
+	}
+}
